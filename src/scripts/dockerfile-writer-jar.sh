163bbf3c1ab2b95c544a7d4c3786efe39582047c
Scope Retail Systems <36751987+scoperetail@users.noreply.github.com>
2021-06-30 12:07:29 -0500

    [semver:minor] - Multiple changes related to release and docker generation
    * changes for various build steps
    
    * changes for various build steps
    
    * changes for various build steps
    
    * changes for various build steps
    
    * changes for various build steps
    
    * changes for various build steps
    
    * changes for various build steps
    
    * changes for various build steps
    
    * changes for various build steps
    
    * changes for various build steps
    
    * changes for various build steps
    
    * changes for various build steps
    
    * changes for various build steps
    
    * changes for various build steps
    
    * changes for various build steps
    
    * changes for various build steps
    
    * changes for various build steps
    
    * changes for various build steps
    
    * changes for various build steps
    
    * changes for various build steps
    
    * changes for various build steps
    
    * changes for various build steps
    
    * changes for various build steps
    
    * changes for various build steps
    
    * changes for various build steps
    
    * bug fix for publishing parent pom files
    
    * bug fix for publishing parent pom files
    
    * bug fix for publishing parent pom files
    
    * bug fix for publishing parent pom files
    
    * Fix docker compose version
    
    * Fix docker compose version
    
    * Fix docker compose version
    
    * Fix docker compose version
    
    * Fix merge
    
    * mvn release:clean
    
    * mvn release:clean
    
    * docker file
    
    * docker file
    
    * docker file
    
    * docker file
    
    * docker file
    
    * docker file
    
    * docker file
    
    * docker file
    
    * docker file
    
    * docker file
    
    Co-authored-by: scoperetail2 <felix.diaz@scoperetail.com>
    Co-authored-by: FelixDiazScope <54597498+FelixDiazScope@users.noreply.github.com>

---
+++ src/scripts/dockerfile-writer-jar.sh
@@ -0,0 +1,29 @@
+#!/bin/bash 
+###################JAR###########################################
+write_dockerfile_jar(){
+cat >~/Dockerfile <<EOF
+#This file was writted by marvelorb in dockerfile-writer.sh
+# add the fat jar file to the base image:
+FROM adoptopenjdk/openjdk11:jdk-11.0.11_9-alpine as builder
+COPY app.jar application.jar
+# extract the layers of the artifact:
+RUN java -Djarmode=layertools -jar application.jar extract
+# copy the extracted folders to add the corresponding Docker layers
+FROM adoptopenjdk/openjdk11:jdk-11.0.11_9-alpine
+RUN mkdir /app & mkdir -p /tmp/fusion/auditor && addgroup -S fusion && adduser -S fusion -G fusion
+COPY --from=builder dependencies/ /app
+COPY --from=builder snapshot-dependencies/ /app
+COPY --from=builder internal-dependencies/ /app
+COPY --from=builder spring-boot-loader/ /app
+COPY --from=builder application/ /app
+RUN chown -R fusion:fusion /app && chmod -R 777 /app
+RUN chown -R fusion:fusion /tmp/fusion && chmod -R 777 /tmp/fusion
+# ENTRYPOINT ["java", "org.springframework.boot.loader.JarLauncher"]
+CMD ["/bin/sh", "-c", "java -cp /app -Dspring.profiles.active=\${SPRING_PROFILE_ACTIVE} org.springframework.boot.loader.JarLauncher \${JAVA_OPTS}"]
+EOF
+echo "Done"
+}
+
+#################INIT###########################################
+echo "INIT writing dockerfile"
+write_dockerfile_jar 
