c6e12cc380ca477a914f60c4b548e837f336339e
scoperetail2 <felix.diaz@scoperetail.com>
2021-07-23 10:08:28 -0500

    fix dockerfile java opts and layers

--- src/scripts/writer-dockerfile.sh
+++ src/scripts/writer-dockerfile.sh
@@ -7,29 +7,26 @@ write_file(){
 cat > "$path/$fileName" <<EOF
 #This file was writted by marvelorb in dockerfile-writer.sh
 # add the fat jar file to the base image:
+
 FROM adoptopenjdk/openjdk11:jdk-11.0.11_9-alpine as builder
 COPY app.jar application.jar
+
 # extract the layers of the artifact:
 RUN mkdir dependencies snapshot-dependencies internal-dependencies spring-boot-loader application
 RUN java -Djarmode=layertools -jar application.jar extract
+
 # copy the extracted folders to add the corresponding Docker layers
 FROM adoptopenjdk/openjdk11:jdk-11.0.11_9-alpine
-RUN mkdir /app & mkdir -p /tmp/fusion/auditor && addgroup -S fusion && adduser -S fusion -G fusion
-COPY --from=builder dependencies/ /app
-COPY --from=builder snapshot-dependencies/ /app
-COPY --from=builder internal-dependencies/ /app
-COPY --from=builder spring-boot-loader/ /app
-COPY --from=builder application/ /app
-RUN chown -R fusion:fusion /app && chmod -R 777 /app
-RUN chown -R fusion:fusion /tmp/fusion && chmod -R 777 /tmp/fusion
-# ENTRYPOINT ["java", "org.springframework.boot.loader.JarLauncher"]
-#CMD ["/bin/sh", "-c", "java -cp /app org.springframework.boot.loader.JarLauncher \${JAVA_OPTS}"]
-##TEPORARY ADDED the jar
-RUN mkdir /home/tomcat 
-COPY app.jar /home/tomcat/app.jar
-RUN chown fusion:fusion /home/tomcat -R 
-USER fusion
-ENTRYPOINT java -jar  /home/tomcat/app.jar
+RUN mkdir -p /home/tomcat  && addgroup -S scope && adduser -S scope -G scope
+
+COPY --from=builder dependencies/ /home/tomcat
+COPY --from=builder snapshot-dependencies/ /home/tomcat
+COPY --from=builder internal-dependencies/ /home/tomcat
+COPY --from=builder spring-boot-loader/ /home/tomcat
+COPY --from=builder application/ /home/tomcat
+RUN chown -R scope:scope /home/tomcat && chmod -R 777 /home/tomcat && ls -la /home/tomcat
+USER scope
+ENTRYPOINT "java -cp /app org.springframework.boot.loader.JarLauncher \${JAVA_OPTS}"
 EOF
 echo "Done"
 }
