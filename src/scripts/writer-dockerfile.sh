f1dc86b66ffc688c7ce1930591165b353ca99a87
FelixDiazScope <54597498+FelixDiazScope@users.noreply.github.com>
2022-04-26 14:18:22 -0500

    [semver:minor] New docker image to avoid vulnerabilities (#16)
    * Upgrade docker image
    
    * Upgrade docker image
    
    * Fix docker adduser
    
    * Fix docker adduser
    
    Co-authored-by: felix.diaz <felix.diaz@alertinnovation.com>

--- src/scripts/writer-dockerfile.sh
+++ src/scripts/writer-dockerfile.sh
@@ -2,13 +2,14 @@
 ##CONFIG
 path="$HOME"
 fileName="Dockerfile"
+
 ###################JAR###########################################
 write_file(){
 cat > "$path/$fileName" <<EOF
 #This file was writted by marvelorb in dockerfile-writer.sh
 # add the fat jar file to the base image:
 
-FROM adoptopenjdk/openjdk11:x86_64-alpine-jre-11.0.13_8 as builder
+FROM openjdk:11.0.14.1-jre-slim-buster as builder
 COPY app.jar application.jar
 
 # extract the layers of the artifact:
@@ -16,8 +17,9 @@ RUN mkdir dependencies snapshot-dependencies internal-dependencies spring-boot-l
 RUN java -Djarmode=layertools -jar application.jar extract
 
 # copy the extracted folders to add the corresponding Docker layers
-FROM adoptopenjdk/openjdk11:x86_64-alpine-jre-11.0.13_8
-RUN mkdir -p /home/tomcat  && addgroup -S scope && adduser -S scope -G scope
+FROM openjdk:11.0.14.1-jre-slim-buster
+RUN mkdir -p /home/tomcat  && addgroup scope && useradd scope -g scope
+
 
 COPY --from=builder dependencies/ /home/tomcat
 COPY --from=builder snapshot-dependencies/ /home/tomcat
